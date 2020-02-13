c8f1ab0fb5a5e70bf737c7ee4a4d46b476dfaf65
Mamh-Linux <bright.ma@blackshark.com>
2020-02-13 10:17:41 +0800

    update JSONChoiceListProvider Constructor and member variables

--- src/main/java/com/cwctravel/hudson/plugins/extended_choice_parameter/JSONChoiceListProvider.java
+++ src/main/java/com/cwctravel/hudson/plugins/extended_choice_parameter/JSONChoiceListProvider.java
@@ -7,8 +7,28 @@ import javax.annotation.Nonnull;
 import java.util.List;
 
 public class JSONChoiceListProvider extends ChoiceListProvider  {
+
+    private String groovyScript;
+    private String bindings;
+    private String groovyClasspath;
+    private String groovyScriptFile;
+
+    private String javascript;
+    private String javascriptFile;
+
+    private boolean saveJSONParameterToFile;
+
+
+
     @DataBoundConstructor
-    public JSONChoiceListProvider() {
+    public JSONChoiceListProvider(String groovyScript, String bindings, String groovyClasspath, String groovyScriptFile, String javascript, String javascriptFile, boolean saveJSONParameterToFile) {
+        this.groovyScript = groovyScript;
+        this.bindings = bindings;
+        this.groovyClasspath = groovyClasspath;
+        this.groovyScriptFile = groovyScriptFile;
+        this.javascript = javascript;
+        this.javascriptFile = javascriptFile;
+        this.saveJSONParameterToFile = saveJSONParameterToFile;
     }
 
     @Override
@@ -25,5 +45,62 @@ public class JSONChoiceListProvider extends ChoiceListProvider  {
         public String getDisplayName() {
             return Messages.JSONChoiceListProvider_DisplayName();
         }
+    }
+
+
+    public String getGroovyScript() {
+        return groovyScript;
+    }
+
+    public void setGroovyScript(String groovyScript) {
+        this.groovyScript = groovyScript;
+    }
+
+    public String getBindings() {
+        return bindings;
+    }
+
+    public void setBindings(String bindings) {
+        this.bindings = bindings;
+    }
+
+    public String getGroovyClasspath() {
+        return groovyClasspath;
+    }
+
+    public void setGroovyClasspath(String groovyClasspath) {
+        this.groovyClasspath = groovyClasspath;
+    }
+
+    public String getGroovyScriptFile() {
+        return groovyScriptFile;
+    }
+
+    public void setGroovyScriptFile(String groovyScriptFile) {
+        this.groovyScriptFile = groovyScriptFile;
+    }
+
+    public String getJavascript() {
+        return javascript;
+    }
+
+    public void setJavascript(String javascript) {
+        this.javascript = javascript;
+    }
+
+    public String getJavascriptFile() {
+        return javascriptFile;
+    }
+
+    public void setJavascriptFile(String javascriptFile) {
+        this.javascriptFile = javascriptFile;
+    }
+
+    public boolean isSaveJSONParameterToFile() {
+        return saveJSONParameterToFile;
+    }
+
+    public void setSaveJSONParameterToFile(boolean saveJSONParameterToFile) {
+        this.saveJSONParameterToFile = saveJSONParameterToFile;
     }
 }
