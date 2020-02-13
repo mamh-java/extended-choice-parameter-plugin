f3b3a56f1f0d800c1cf6de8471a0cd692df004e6
Mamh-Windows <bright.ma@blackshark.com>
2020-02-13 21:13:46 +0800

    add Override newInstance for create new JSONChoiceListProvider

--- src/main/java/com/cwctravel/hudson/plugins/extended_choice_parameter/JSONChoiceListProvider.java
+++ src/main/java/com/cwctravel/hudson/plugins/extended_choice_parameter/JSONChoiceListProvider.java
@@ -2,12 +2,16 @@ package com.cwctravel.hudson.plugins.extended_choice_parameter;
 
 import hudson.Extension;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.StaplerRequest;
+import net.sf.json.JSONObject;
 
 import javax.annotation.Nonnull;
 import java.util.List;
 
-public class JSONChoiceListProvider extends ChoiceListProvider  {
+import static com.cwctravel.hudson.plugins.extended_choice_parameter.ExtendedChoiceParameterDefinition.PARAMETER_TYPE_JSON;
 
+public class JSONChoiceListProvider extends ChoiceListProvider {
+    private String type;
     private String groovyScript;
     private String bindings;
     private String groovyClasspath;
@@ -19,16 +23,9 @@ public class JSONChoiceListProvider extends ChoiceListProvider  {
     private boolean saveJSONParameterToFile;
 
 
-
     @DataBoundConstructor
-    public JSONChoiceListProvider(String groovyScript, String bindings, String groovyClasspath, String groovyScriptFile, String javascript, String javascriptFile, boolean saveJSONParameterToFile) {
-        this.groovyScript = groovyScript;
-        this.bindings = bindings;
-        this.groovyClasspath = groovyClasspath;
-        this.groovyScriptFile = groovyScriptFile;
-        this.javascript = javascript;
-        this.javascriptFile = javascriptFile;
-        this.saveJSONParameterToFile = saveJSONParameterToFile;
+    public JSONChoiceListProvider() {
+
     }
 
     @Override
@@ -45,8 +42,70 @@ public class JSONChoiceListProvider extends ChoiceListProvider  {
         public String getDisplayName() {
             return Messages.JSONChoiceListProvider_DisplayName();
         }
+
+
+        @Override
+        public JSONChoiceListProvider newInstance(StaplerRequest req, JSONObject formData) throws FormException {
+            JSONChoiceListProvider provider = (JSONChoiceListProvider) super.newInstance(req, formData);
+            boolean saveJSONParameterToFile = false;
+
+            String groovyScript = null;
+            String groovyScriptFile = null;
+            String bindings = null;
+            String groovyClasspath = null;
+            String javascriptFile = null;
+            String javascript = null;
+
+
+            provider.setType(PARAMETER_TYPE_JSON);
+            JSONObject json1 = (JSONObject) formData.get("jsonParameterConfigSource");
+            if (json1 != null) {
+                if (json1.getInt("value") == 0) {
+                    groovyScript = json1.getString("groovyScript");
+                    groovyScriptFile = null;
+                    bindings = json1.getString("bindings");
+                    groovyClasspath = json1.getString("groovyClasspath");
+                } else if (json1.getInt("value") == 1) {
+                    groovyScript = null;
+                    groovyScriptFile = json1.getString("groovyScriptFile");
+                    bindings = json1.getString("bindings");
+                    groovyClasspath = json1.getString("groovyClasspath");
+                }
+            }
+            provider.setGroovyScriptFile(groovyScriptFile);
+            provider.setGroovyScript(groovyScript);
+            provider.setBindings(bindings);
+            provider.setGroovyClasspath(groovyClasspath);
+
+
+            JSONObject json2 = (JSONObject) formData.get("jsonParameterConfigJavascriptSource");
+            if (json2 != null) {
+                if (json2.getInt("value") == 0) {
+                    javascript = json2.optString("javascript");
+                    javascriptFile = null;
+                } else if (json2.getInt("value") == 1) {
+                    javascriptFile = json2.optString("javascriptFile");
+                    javascript = null;
+                }
+            }
+            provider.setJavascript(javascript);
+            provider.setJavascriptFile(javascriptFile);
+
+            saveJSONParameterToFile = formData.optBoolean("saveJSONParameterToFile");
+            provider.setSaveJSONParameterToFile(saveJSONParameterToFile);
+
+
+            return provider;
+        }
     }
 
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 
     public String getGroovyScript() {
         return groovyScript;
